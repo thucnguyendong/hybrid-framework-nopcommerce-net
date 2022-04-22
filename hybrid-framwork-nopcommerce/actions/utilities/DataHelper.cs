5e2b25b635685fb9ecf9f7e35de9bcd38262aa80
Nguyen Dong Thuc <nguyendongthuc13@gmail.com>
2022-04-22 21:28:22 +0700

    Update Nop-commerce framework + Add DataHelper class to generate fake data + Add RegisterData class to read from json file

---
+++ hybrid-framwork-nopcommerce/actions/utilities/DataHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hybrid_framwork_nopcommerce.actions.utilities
+{
+    public class DataHelper
+    {
+        public static DataHelper GetData()
+        {
+            return new DataHelper();
+        }
+
+        public string GetFirstName()
+        {
+            return Faker.Name.First();
+        }
+
+        public string GetLastName()
+        {
+            return Faker.Name.First();
+        }
+
+        public string GetEmail()
+        {
+            return Faker.Internet.Email();
+        }
+
+        public string GetAddress()
+        {
+            return Faker.Address.StreetAddress();
+        }
+    }
+}
---
+++ hybrid-framwork-nopcommerce/testdata/com.nopcommerce.userdata/RegisterData.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace hybrid_framwork_nopcommerce.testdata.com.nopcommerce.userdata
+{
+    public class RegisterData
+    {
+        public void LoadJson()
+        {
+            using StreamReader r = new StreamReader("RegisterData.json");
+            string json = r.ReadToEnd();
+            List<Register> items = JsonConvert.DeserializeObject<List<Register>>(json);
+        }
+
+        public class Register
+        {
+            public string firstname;
+            public string lastname;
+            public string company;
+            public string password;
+            public IList<string> Dob { get; set; }
+        }
+    }
+}
